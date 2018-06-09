6ce0d719853013661acb51ee73cb531ea1b9673f
Jozue06 <jozue06@gmail.com>
2018-06-09 12:05:44 -0700

    gets work for good and err

--- __test__/src/api/api.test.js
+++ __test__/src/api/api.test.js
@@ -1,19 +1,30 @@
 const superagent = require('superagent');
+require('/Users/joshuamcclung/codefellows/401/09-rest-persistence/src/api/api.js');
 
 describe('app',() =>{
 
-  it('should', () =>{
+  it('should post a note with id', () =>{
+    let obj = {
+      content: 'freds name', 
+      title: 'Fred'};
+    let expected;
+    return superagent
+      .post('http://localhost:3333/api/v1/notes')
+      .send(obj)
+      .then(data => expect(expected).toEqual(data.body.id))
+
+      .catch(err => {return err;});
+  });
 
 
-    // const expected = 'ID: 123';
-    // expect(actual).toBe
-    superagent
-      .get('http://localhost:3333/api/v1/drums?id=123')
-      .then(data=> console.log(data))
-      .catch(err => console.log(err));
+  it('should get back a note with specific id', () => {
 
-
-
+    return superagent
+      .get('http://localhost:3333/api/v1/notes?id=')
+      .then(data => {
+        expect(data.statusCode).toBe(404);
+      } )
+      .catch(err => {return err;});
   });
 
 
--- src/api/api.js
+++ src/api/api.js
@@ -25,12 +25,35 @@ let serverError = (res,err) => {
   res.end();
 };
 
+let routesError = (res, err) => {
+  let error = {error:err};
+  res.statusCode = 404;
+  res.statusMessage = 'Connot find ID';
+  res.setHeader('Content-Type', 'application/json');
+  res.write( JSON.stringify(error) );
+  res.end();
+};
+
+let routesErrorId = (res) => {
+  res.statusCode = 400;
+  res.statusMessage = 'bad request';
+  res.setHeader('Content-Type', 'application/json');
+  res.end();
+};
+
+
 router.get('/api/v1/notes', (req,res) => {
-  if ( req.query.id ) {
+
+  if(req.query.id === ''){
+    routesErrorId(res);
+  }
+
+  if ( req.query.id ) { 
     Notes.findOne(req.query.id)
       .then( data => sendJSON(res,data) )
-      .catch( err => serverError(res,err) );
+      .catch( err => routesError(res,err) );
   }
+
   else {
     Notes.fetchAll()
       .then( data => sendJSON(res,data) )
@@ -67,73 +90,15 @@ router.post('/api/v1/notes', (req,res) => {
 
 });
 
+router.put('/api/v1/notes', (req,res) => {
+  if ( req.query.id ) {
+    Notes.updateOne(req.query.id)
+      .then( success => {
+        let data = {id:req.query.id,updated:success};
+        sendJSON(res,data);
+      })
+      .catch(console.error);
+  }
+});
+
 module.exports = {};
-
-
-// 'use strict';
-
-// const router = require('../lib/router.js');
-
-// function routesHeader (res){
-//   res.statusCode = 200;
-//   res.statusMessage = 'OK';
-// }
-
-
-
-// router.get('/api',(req,res) =>{
-//   res.statusCode = 400;
-//   res.statusMessage = 'bad request';
-//   res.end();
-// });
-
-// router.get('/api/v1/drums', (req, res)=>{
-//   if(!req.query.id){
-//     res.statusCode = 404;
-//     res.statusMessage = 'not found';
-//     return err;
-//   }
-//   else{
-//     routesHeader(res);
-//     res.write(`drum info for ID: ${req.query.id}`);
-//     res.end();
-//   }
-// });
-
-// // test with httpie:
-// //    echo '{"title":"Go Home","content":"foobar"}' | http post http://localhost:3333/data
- 
-// router.post('/api/v1/drums', (req,res) => {
-//   if(!req.body){
-//     res.statusCode = 500;
-//     res.statusMessage = 'no such record';
-//     return err;}
-//   else{
-//     // routesHeader(res);
-//     res.write( JSON.stringify(req.body) );
-//     res.end();
-//   }
-// });
-
-// router.put('/api/v1/drums', (req,res) => {
-//   if(!req.query.id){return err;}
-//   else if(!req.body){return err;}
-//   else{
-//     routesHeader(res);  
-//     res.write(JSON.stringify(req.body));
-//     res.end();
-//   }
-  
-// });
-
-
-// router.delete('/api/v1/drums', (req,res) => {
-//   routesHeader(res);
-//   res.write(`id: ${req.query.id} was deleted`);
-//   res.end();
-// });
-
-
-
-// module.exports = {};
-
