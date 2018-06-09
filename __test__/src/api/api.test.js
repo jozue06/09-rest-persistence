8b5250c68f14c2465589ca14b0d9a128493a201b
Jozue06 <jozue06@gmail.com>
2018-06-09 16:22:21 -0700

    lint2

--- __test__/src/api/api.test.js
+++ __test__/src/api/api.test.js
@@ -1,6 +1,6 @@
 const superagent = require('superagent');
-require('/Users/joshuamcclung/codefellows/401/09-rest-persistence/src/api/api.js');
-const app = require('/Users/joshuamcclung/codefellows/401/09-rest-persistence/src/app.js');
+require('../../../src/api/api.js');
+const app = require('../../../src/app.js');
 
 
 /*
@@ -8,11 +8,11 @@ const app = require('/Users/joshuamcclung/codefellows/401/09-rest-persistence/sr
 * write a test to ensure that your api returns a status code of 404 for routes that have not been registered
 * write tests to ensure the `/api/simple-resource-name` endpoint responds as described for each condition below:
 
- * `GET`: test 404, it should respond with 'not found' for valid requests made with an id that was not found
- * `GET`: test 400, it should respond with 'bad request' if no id was provided in the request
- * `GET`: test 200, it should contain a response body for a request made with a valid id
+ * `GET`: test 404, it should respond with 'not found' for valid requests made with an id that was not found [ X ]
+ * `GET`: test 400, it should respond with 'bad request' if no id was provided in the request [ X ]
+ * `GET`: test 200, it should contain a response body for a request made with a valid id [ X ]
  * `POST`: test 400, it should respond with 'bad request' if no request body was provided or the body was invalid
- * `POST`: test 200, it should respond with the body content for a post request with a valid body
+ * `POST`: test 200, it should respond with the body content for a post request with a valid body [ X ]
  * 
 
 */
